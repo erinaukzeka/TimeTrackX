e33e93ad9305d05a18a5d3cc03517cd7e238d749
nesaa-a <agnesahalimii@outlook.com>
2025-05-16 16:26:38 +0200

    login updated

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import authRouter from './routers/auth.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('/api/auth', authRouter)
 
 app.listen(process.env.PORT, () => {
-     console.log('Server is running on port ${process.env.PORT}')
+     console.log(`Server is running on port ${process.env.PORT}`)
 })
