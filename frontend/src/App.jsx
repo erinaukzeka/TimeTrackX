84c31c649a89cdc2358c86fb05a46b108cb28014
nesaa-a <agnesahalimii@outlook.com>
2025-05-19 12:51:56 +0200

    Admin Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import AdminDashboard from './pages/AdminDashboard';
+import EmployeeDashboard from './pages/EmployeeDashboard';
+import PrivateRouters from './utils/PrivateRoutes';
+import RoleBaseRoutes from './utils/RoleBaseRoutes';
+import AdminSummary from './components/dashboard/AdminSummary';
+import DepartmentList from './components/department/DepartmentList';
 
 function App() {
 
@@ -9,7 +14,19 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/admin-dashboard" />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
+        <Route path="/admin-dashboard" element={
+          <PrivateRouters>
+            <RoleBaseRoutes requiredRole={["admin"]}>
+              <AdminDashboard />
+            </RoleBaseRoutes>
+          </PrivateRouters>
+          }>
+          <Route index element={<AdminSummary />}></Route>
+          
+          <Route path="/admin-dashboard/departments" element={<DepartmentList />}></Route>
+
+          </Route>
+        <Route path="/employee-dashboard" element={<EmployeeDashboard />}></Route>
     </Routes>
     </BrowserRouter>
   )
--- frontend/src/context/authContext.jsx
+++ frontend/src/context/authContext.jsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
 const authContext = () => {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+  const verifyUser = async () => {
+    try{
+      const token = localStorage.getItem("token");
+      if(token){
+        const response = await axios.get(
+          "http://localhost:5000/api/auth/verify",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        if (response.data.success) {
+          setUser(response.data.user);
+        }
+      }else{
+        setUser(null);
+        setLoading(false);
+      }
+    } catch (error) {
+      if (error.response && !error.response.data.error){
+        setUser(null)
+      }
+    }
+  }
+})
 
     const login = a
   return (
