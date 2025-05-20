abc87e6df8f13ff6e77acad983ddcf47d0c4fdf6
nesaa-a <agnesahalimii@outlook.com>
2025-05-20 22:55:05 +0200

    Updated

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
-import express from 'express'
-import cors from 'cors'
-import authRouter from './routes/auth.js'
-import connectToDatabase from './db/db.js'
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/auth.js';
+import connectToDatabase from './db/db.js';
+import dotenv from 'dotenv';
 
+dotenv.config(); 
 
-connectToDatabase()
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use('/api/auth', authRouter)
+const app = express();
 
-app.listen(process.env.PORT, () => {
-     console.log(`Server is running on port ${process.env.PORT}`)
-})
+connectToDatabase(); 
 
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+``
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
